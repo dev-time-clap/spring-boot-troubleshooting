e6801c92e8028e6ae0e08d83c165e6274732c4fc
Morrigan <morrigan@dev-time.de>
2026-01-06 22:20:50 +0100

    feat(troubleshooting) mockito agent warning solution added

---
+++ episode-0001/library/src/test/java/de/devtime/examples/library/DemoBibliothekApplicationIT.java
@@ -0,0 +1,13 @@
+package de.devtime.examples.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBibliothekApplicationIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
